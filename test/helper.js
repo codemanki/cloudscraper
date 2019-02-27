d574123940a997ce71c9d42186917b82435e8419
pro-src <rodneyd.teal@gmail.com>
2019-02-27 03:08:26 -0600

    Rewrite all tests

--- test/helper.js
+++ test/helper.js
@@ -1,45 +1,79 @@
-var fs = require('fs');
-var urlLib = require('url');
-var path = require('path');
+var request = require('request');
+var sinon   = require('sinon');
+var fs      = require('fs');
+var url     = require('url');
+var path    = require('path');
 
-var testDefaults = {
-  url: 'http://example-site.dev/path/',
-  headers: {'User-Agent': 'Chrome'}
+var defaultParams = {
+  // Since cloudscraper wraps the callback, just ensure callback is a function
+  callback: sinon.match.func,
+  jar: request.jar(),
+  uri: 'http://example-site.dev/path/',
+  headers: {
+    "User-Agent": "Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36",
+    "Cache-Control": "private",
+    "Accept": "application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5"
+  },
+  method: 'GET',
+  encoding: null,
+  realEncoding: 'utf8',
+  followAllRedirects: true,
+  cloudflareTimeout: 6000,
+  challengesToSolve: 3
 };
+
+// Cache fixtures so they're not read from the fs but once
+var cache = {};
 
 module.exports = {
   getFixture: function(fileName) {
-    return fs.readFileSync('./specs/fixtures/' + fileName, 'utf8');
-  },
-  testDefaults: testDefaults,
-  // This method returns properly faked response object for request lib, which is used inside cloudscraper library
-  fakeResponseObject: function(statusCode, headers, body, url) {
-    var parsedUri = urlLib.parse(url);
-    parsedUri.uri = parsedUri;
-
-    return {
-      statusCode: statusCode,
-      headers: headers,
-      body: body,
-      request: parsedUri //actually this is more compilcated object, but library uses only uri parts.
-    };
-  },
-  // Terrible hack. But because of request library API, it is impossible to normally stub it. That is why cloudscraper's index.js is removed from cache each time
-  dropCache: function() {
-    var pathToLib = path.normalize(__dirname + '/../index.js');
-    if (require.cache[pathToLib]) {
-      delete require.cache[pathToLib];
+    if (cache[fileName] === undefined) {
+      // noinspection JSUnresolvedVariable
+      cache[fileName] = fs.readFileSync(path.join(__dirname, 'fixtures', fileName), 'utf8');
     }
+    return cache[fileName];
   },
-  requestParams: function(params) {
-    return Object.assign({
-      method: 'GET', 
-      url: testDefaults.url, 
-      headers: testDefaults.headers,
-      encoding: null, 
-      realEncoding: 'utf8', 
-      followAllRedirects: true,
-      challengesToSolve: 3
-    }, params);
+  defaultParams: defaultParams,
+  // This method returns properly faked response object for request lib, which is used inside cloudscraper library
+  fakeResponse: function(template) {
+    var fake = Object.assign({
+      statusCode: 200,
+      headers: defaultParams.headers,
+      body: '',
+    }, template);
+
+    // The uri property of the fake response is only for tests to simplify fake request creation.
+    var uri = url.parse(fake.uri || defaultParams.uri);
+    // The actual request object is more complicated but this library only uses the uri parts.
+    fake.request = {
+      host: uri.host,
+      uri: uri
+    };
+
+    return fake;
+  },
+  extendParams: function(params) {
+    // Extend target with the default params and provided params
+    var target = Object.assign({}, defaultParams, params);
+    // Extend target.headers with defaults headers and provided headers
+    target.headers = Object.assign({}, defaultParams.headers, params.headers);
+    return target;
+  },
+  fakeRequest: function(template) {
+    // In this context, fake is the request result
+    var fake = Object.assign({
+      error: null,
+      // Set the default fake statusCode to 500 if an error is provided
+      response: { statusCode: template.error ? 500 : 200 },
+    }, template);
+
+    // Use the body from fake response if the template doesn't provide it
+    if (!('body' in fake)) {
+      fake.body = fake.response.body;
+    }
+
+    return function Request(params) {
+      params.callback(fake.error, fake.response, fake.body);
+    };
   }
 };
