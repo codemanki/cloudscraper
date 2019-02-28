0631031ad2fb3de384db17c0b364f43266c6e4a0
pro-src <rodneyd.teal@gmail.com>
2019-02-27 20:46:06 -0600

    Remove always false if statement

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ function processRequestResponse(options, requestResult, callback) {
   var isRedirectChallengePresent;
 
   if (error || !body || !body.toString) {
+    // Pure request error (bad connection, wrong url, etc)
     return callback({ errorType: 0, error: error }, response, body);
   }
 
@@ -103,11 +104,6 @@ function processRequestResponse(options, requestResult, callback) {
 
 function checkForErrors(error, body) {
   var match;
-
-  // Pure request error (bad connection, wrong url, etc)
-  if(error) {
-    return { errorType: 0, error: error };
-  }
 
   // Finding captcha
   if (body.indexOf('why_captcha') !== -1 || /cdn-cgi\/l\/chk_captcha/i.test(body)) {
